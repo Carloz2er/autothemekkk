bdbe7f92895fefb99fb4f2728908265f4ffd31e3
Carloz2er <73202676+Carloz2er@users.noreply.github.com>
2022-03-13 22:39:11 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,86 +1,12 @@
-#!/bin/bash
+declare -A cpuinfo=( ) # create an associative array
 
-set -e
+# read line-by-line; see http://mywiki.wooledge.org/BashFAQ/001
+while IFS=$'\t:' read -r k v || [[ $k ]]; do
+  [[ $v ]] || continue
+  cpuinfo[$k]=${v# } # trim leading space; see http://wiki.bash-hackers.org/syntax/pe
+done </proc/cpuinfo
 
-# CarlozCode
-
-print_brake() {
-  for ((n = 0; n < $1; n++)); do
-    echo -n "#"
-  done
-  echo ""
-}
-
-hyperlink() {
-  echo -e "\e]8;;${1}\a${1}\e]8;;\a"
-}
-
-RESET="\e[0m"
-RED='\033[0;31m'
-
-error() {
-  echo -e "* ${RED}FALHA:${RESET}: $1"
-}
-if [[ $EUID -ne 0 ]]; then
-  echo "* Entre no modo "Sudo" para executar este script! 1>&2
-  exit 1
-fi
-if ! [ -x "$(command -v curl)" ]; then
-  echo "* "
-exit 1
-fi
-
-cancel() {
-echo
-echo -e "* ${RED}Cancelando... ${RESET}"
-done=true
-exit 1
-}
-
-done=false
-
-echo
-print_brake 70
-echo "* CarlozCodes - AutoScript Para Pterodactyl"
-echo
-print_brake 70
-echo
-
-Backup() {
-bash <(curl -s https://raw.githubusercontent.com/Carloz2er/autothemekk/main/backup.sh)
-}
-
-Dracula() {
-bash <(curl -s https://raw.githubusercontent.com/Carloz2er/autothemekkk/main/theme.sh)
-}
-
-while [ "$done" == false ]; do
-  options=(
-    "Usar o backupinfeito"
-    "Tema Dracula"
-    "Cancelar"
-  )
-  
-  actions=(
-    "Backup"
-    "Tema"
-    "Cancelar"
-  )
-  
-  echo "* Which theme do you want to install?"
-  echo
-  
-  for i in "${!options[@]}"; do
-    echo "[$i] ${options[$i]}"
-  done
-  
-  echo
-  echo -n "* Input 0-$((${#actions[@]} - 1)): "
-  read -r action
-  
-  [ -z "$action" ] && error "Input is required" && continue
-  
-    valid_input=("$(for ((i = 0; i <= ${#actions[@]} - 1; i += 1)); do echo "${i}"; done)")
-  [[ ! " ${valid_input[*]} " =~ ${action} ]] && (error "Invalid option")
-  [[ " ${valid_input[*]} " =~ ${action} ]] && (done=true && eval "${actions[$action]}")
-done
+echo "Model name: ${cpuinfo['model name']%@*}"
+echo "Model freq: ${cpuinfo['model name']#*@}"
+echo "Actual frequency: ${cpuinfo['cpu MHz']}"
+echo "Siblings: ${cpuinfo['siblings']}"
